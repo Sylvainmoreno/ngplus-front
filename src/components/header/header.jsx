b6fd80e9b7f8797f81159d3887207fcd092aa52c
Flo-droid <florianplot@msn.com>
2020-03-04 15:47:54 +0100

    skeleton homepage

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText } from 'reactstrap';
 import '../../css/Header.css'
-import { Link } from 'react-router-dom';
 import Mario from '../../images/mario.gif';
 
 const Header = () => {
--- src/components/maincontainer/homepage/homepage.jsx
+++ src/components/maincontainer/homepage/homepage.jsx
@@ -4,7 +4,28 @@ import '../../../css/homepage.css';
 
 const Homepage = () => {
     return (
-        <div>
+        <div className="homepage">
+
+            <div className="margin">
+
+                <div className="slide">
+                </div>
+
+                <div className="actupro">
+                    <div className="big">
+                    </div>
+                    <div className="small">
+                    </div>
+                </div>
+
+                <div className="actupro">
+                    <div className="big">
+                    </div>
+                    <div className="small">
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     )
